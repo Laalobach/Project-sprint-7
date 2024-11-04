8ffdfc3d717c3ac90a5cf9ce4a56dcbeec9abd19
Alexandr <laalobach@gmail.com>
2024-11-04 23:23:17 +0300

    second_iteration

--- precode_test.go
+++ precode_test.go
@@ -1,58 +1,47 @@
 package main
 
 import (
-    "net/http"
-    "net/http/httptest"
-    "testing"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"io"
-	"fmt"
-	"strings"
 )
-type CafeClient struct {
-	client *http.Client
-	baseURL string
-}
-var cafeClient CafeClient
 
-func init() {
-	server := httptest.NewServer(http.HandlerFunc(mainHandle))
-	cafeClient = CafeClient{server.Client(), server.URL}
-}
-func TestCorrectRequest(t *testing.T){
-	response := cafeClient.sentRequest("/cafe?count=4&city=moscow")
+func TestCorrectRequest(t *testing.T) {
+	response := sentRequest("/cafe?count=4&city=moscow")
+	body := response.Body.String()
 
-	body, _ := io.ReadAll(response.Body)
-	defer response.Body.Close()
-	require.Equal(t, 200, response.StatusCode)
+	require.Equal(t, http.StatusOK, response.Code)
 	assert.NotEmpty(t, body)
 }
-func TestWrongCity(t *testing.T){
-	response := cafeClient.sentRequest("/cafe?count=4&city=Sevastopol")
-	body, _ := io.ReadAll(response.Body)
-	defer response.Body.Close()
+func TestWrongCity(t *testing.T) {
+	response := sentRequest("/cafe?count=4&city=Sevastopol")
+	body := response.Body.String()
 	errorMessage := string(body)
 
-	require.Equal(t, 400, response.StatusCode)
-	require.Equal(t, errorMessage, "wrong city value")
+	require.Equal(t, http.StatusBadRequest, response.Code)
+	assert.Equal(t, errorMessage, "wrong city value")
 }
 
 func TestMoreThanExistCount(t *testing.T) {
-	response := cafeClient.sentRequest("/cafe?count=10&city=moscow")
-	body, _ := io.ReadAll(response.Body)
-	defer response.Body.Close()
+	response := sentRequest("/cafe?count=10&city=moscow")
+	body := response.Body.String()
+	totalCount := 4
+	listResponse := strings.Split(body, ",")
 
-    listResponse := strings.Split(string(body), ",")
-	cafeListMoscow:= cafeList["moscow"]
-
-	require.Equal(t, 200, response.StatusCode)
-	require.Len(t, cafeListMoscow, len(listResponse))
+	require.Equal(t, http.StatusOK, response.Code)
+	assert.Len(t, listResponse, totalCount)
 }
 
-func (c *CafeClient) sentRequest(endpoint string) *http.Response{
-	request, _ := http.NewRequest("GET", c.baseURL + endpoint, nil)
-	response, err := c.client.Do(request)
-	fmt.Println(err)
-	return response
+func sentRequest(endpoint string) *httptest.ResponseRecorder {
+	request, _ := http.NewRequest("GET", endpoint, nil)
+
+	responseRecorder := httptest.NewRecorder()
+    handler := http.HandlerFunc(mainHandle)
+    handler.ServeHTTP(responseRecorder, request)
+	
+	return responseRecorder
 }
